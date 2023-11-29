bd69db2b68671b7b0a1ab654c086bb3c89eac7f0
NHTfuntin31 <funtin3110@gmail.com>
2023-11-29 12:40:45 +0900

    Urls config and view

--- it81api/ecourses/courses/views.py
+++ it81api/ecourses/courses/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 #
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 def index(request):
 	return render(request, template_name='index.html', context = {
 		'name' : 'inosuke'
 	})
+
+
+def welcome(request, year):
+    return HttpResponse("hello" + str(year))
+
+def welcome2(request, year):
+    return HttpResponse("hello2" + str(year))
+
+class TestView(View):
+    def get(self, request):
+        return HttpResponse("Testing")
+    
+    def post(self, request):
+        pass
